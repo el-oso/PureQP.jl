1f9b734752892c2c1f750ca5f5b1f26e05f9bb11
Documenter.jl <documenter@juliadocs.github.io>
2026-09-20 21:50:49 +0000

    build based on 8be950d

--- dev/assets/chunks/swimlanesDiagram-VR7AAH4N.uoDgE12c.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW.hrnXGdTe.js";import{_ as a}from"../app.D_5fd0X5.js";import"./chunk-5VM5RSS4.DcSe5b-0.js";import"./chunk-XXDRQBXY.DA5yiuvb.js";import"./chunk-POPQ4Y6H.BApB1Z8s.js";import"./chunk-F27PBJKO.BRB4mD2d.js";import"./channel.nrcVkufj.js";import"./framework.BAh_9QZx.js";import"./theme.C9vNs2Jw.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
